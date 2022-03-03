58f2c3f4e44f00e1ffd11892fc2b3555686a956a
pedro moreira <jpedrostonemoreira@gmail.com>
2022-03-02 23:02:39 -0300

    uploado application.php com classes dinamicas

--- Application.php
+++ Application.php
@@ -1,8 +1,31 @@
 <?php
 
 	class Application{
+		const DEFAULT = 'Login';
+
 		public function run(){
-			echo 'Chamando run da classe aplication';
+			if(isset($_GET['url'])){
+				$url = explode('/', $_GET['url']);
+				$class = 'controllers\\'.ucfirst($url[0]).'Controller';
+			}else{
+				$class = 'controllers\\'.self::DEFAULT.'Controller';
+				$url[0] = self::DEFAULT;
+			}
+
+
+			$view = 'views\\'.ucfirst($url[0]).'View';
+			$model = 'models\\'.ucfirst($url[0]).'Model';
+
+			echo $class;
+			echo '<br />';
+			echo $view;
+			echo '<br />';
+			echo $model;
+
+
+			//$controller = new $class(new $view, new $model);
+			//$controller->index();
+
 		}
 	}
 
